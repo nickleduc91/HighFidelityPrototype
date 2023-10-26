283341cfd0e94bd4e25353121822e01de3f27fed
nickleduc91 <122701320+nickleduc91@users.noreply.github.com>
2023-10-26 13:56:15 -0400

    re-did the cards

--- components/saved.jsx
+++ components/saved.jsx
@@ -52,23 +52,18 @@ const Saved = () => {
               key={index}
               className="pointer-events-none rounded-3xl border-2 h-40 w-64"
             >
-              <div className="grid grid-cols-3 place-items-end">
-                <div className="col-span-1">
-                  <img className="object-contain w-full h-full" src={item.image} />
-                </div>
-                <div className="col-span-2 bg-gray-200 h-40 rounded-r-3xl pt-1">
-                  <p className="font-bold">{item.name}</p>
-                  <p>
-                    <i className="ri-map-pin-line"></i>
-                    {item.address}
-                  </p>
-                  <p>
-                    <i className="ri-star-line pr-1"></i>
-                    {item.rating}/5
-                  </p>
-                  <p>{item.storeType}</p>
-                  <p className="mt-8">${item.price} CAD</p>
-                </div>
+              <div className="col-span-2 bg-white h-40 rounded-3xl pt-1 shadow-2xl">
+                <p className="font-bold pl-4">{item.name}</p>
+                <p>
+                  <i className="ri-map-pin-line pl-4"></i>
+                  {item.address}
+                </p>
+                <p>
+                  <i className="ri-star-line pr-1 pl-4"></i>
+                  {item.rating}/5
+                </p>
+                <p className="pl-4">{item.storeType}</p>
+                <p className="mt-8 pl-4">${item.price} CAD</p>
               </div>
             </div>
           ))}
